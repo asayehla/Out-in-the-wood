b825a6150317be63121457747c8bda3a5b540ac2
Åsa Palholmen <asa.palholmen@humblebee.se>
2020-02-06 13:29:47 +0100

    added header

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import {
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-      </Switch>
+    <div>
+      <Router>
+      <Header />
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
       </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
